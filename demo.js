046484c87073caa281d0dfd2c719d8ea8cbf73ee
Deadlocked <korotayev05@mail.ru>
2024-11-09 23:48:13 +0500

    Создание репозитория и реализация EndPoints

--- demo.js
+++ demo.js
@@ -13,4 +13,44 @@ class Order {
       this.status = status;
     }
   }
+let repo = [
+  {
+    number: 1,
+    date: "02-02-2005",
+    device: "Monitor",
+    problemType: "Сбой",
+    description: "help",
+    client: "Ivan Zolo",
+    status: "В ожидании"    
+   }
+];
+  
+app.use(cors()); 
+app.get("/orders", (req, res) => {
+res.send(repo);
+});
+app.post("/ord", jsonInterpretator, (req, res) => {
+const {
+        number,
+        date,
+        device,
+        problemType,
+        description,
+        client,
+        status,
+    } = req.body;
+
+const newOrder = new Order(
+    number,
+    date,
+    device,
+    problemType,
+    description,
+    client,
+    status
+  );
+  repo.push(newOrder);
+  res.send(newOrder);
+});
+app.listen(7007);
 app.listen(7007);
